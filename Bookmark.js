8a35e1c93615ebac04f9971cc48acaf585dfcc22
Mahadiputra <adi_a12@yahoo.com>
2017-09-01 20:37:03 +0700

    update

--- Bookmark.js
+++ Bookmark.js
@@ -1,7 +1,9 @@
-javascript:
-var keys = prompt('Insert steam key here!');
+javascript: 
+var keys = prompt('WARNING\nYou will be directed to the activation page and losing this page.\n\nInsert your steam key here...!');
 if (keys != null){
+	if (keys == ''){alert('No string detected....!');}
 	if (keys != ''){
-		window.location.href = 'https://store.steampowered.com/account/registerkey?key=' + keys;
+	window.location.href = 'https://store.steampowered.com/account/registerkey?key=' + keys;
 	}
 }
+else alert('Bye......♥');
