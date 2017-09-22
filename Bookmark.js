279b3f571b03164933faf62ea46ca665e3e03387
Mahadiputra <adi_a12@yahoo.com>
2017-09-22 22:32:31 +0700

    update bookmark.js

--- Bookmark.js
+++ Bookmark.js
@@ -1,9 +1,17 @@
-javascript: 
-var keys = prompt('WARNING\nYou will be directed to the activation page and losing this page.\n\nInsert your steam key here...!');
-if (keys != null){
-	if (keys == ''){alert('No string detected....!');}
-	if (keys != ''){
-	window.location.href = 'https://store.steampowered.com/account/registerkey?key=' + keys;
+javascript:
+var selects = window.getSelection().toString();
+if (selects) {
+	var sel2 = selects.match(/((((?![osuOSU])[a-zA-z1-9]){5}-){2}){1,2}((?![osuOSU])[a-zA-z1-9]){5}$|((?![osuOSU])[a-zA-z1-9]){15}$/g);
+	if (sel2 != null){ location.href = 'https://store.steampowered.com/account/registerkey?key=' + selects;
 	}
+	else { alert('Its not a key.\nBye......♥');}
+	}
+else {
+	keys = prompt('WARNING\nYou will be directed to the activation page and losing this page.\n\nInsert your steam key here...!'); 
+	if(keys != null){
+		var keys2 = keys.match(/((((?![osuOSU])[a-zA-z1-9]){5}-){2}){1,2}((?![osuOSU])[a-zA-z1-9]){5}$|((?![osuOSU])[a-zA-z1-9]){15}$/g);
+		if (keys == '' | keys2 == null){ alert('No Valid Key Detected....!');} 
+		if (keys == keys2){ window.location.href = 'https://store.steampowered.com/account/registerkey?key=' + keys; }
+	}
+	else { alert('Bye......♥');}
 }
-else alert('Bye......♥');
